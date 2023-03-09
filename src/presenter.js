aaaec156c88377a1ee16fc0feca6efc62046f356
Mateo <mateo>
2023-03-09 05:30:14 -0800

    ingresar titulo y descripcion pero no se puede mostrar todavia

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,56 @@
-import sumar from "./sumador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const formPublicacion = document.querySelector("#publicacion-form");
+const titulo = document.querySelector("#titulo");
+const descripcion = document.querySelector("#descripcion");
+const divPublicaciones = document.querySelector("#publicacion-div");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+const objPublicacion = {
+  fecha: "",
+  titulo: "",
+  texto: "",
+};
+let listaPublicaciones = [];
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
-});
+function crearPublicacion() {
+    listaPublicaciones.push({ ...objPublicacion });
+    mostrarPublicaciones();
+    formPublicacion.reset();
+    limpiarObjeto();
+ }
+
+formPublicacion.addEventListener("submit", validarFormulario);
+
+function validarFormulario(e) {
+ e.preventDefault();
+ objPublicacion.titulo = tituloInput.value;
+ objPublicacion.descripcion = descripcionInput.value;
+ crearPublicacion();
+}
+
+
+function mostrarPublicaciones() {
+  refrescarHTML();
+  const divPublicaciones = document.querySelector("#publicacion-div");
+  listaPublicaciones.forEach((publicacion) => {
+    const { titulo, descripcion } = publicacion;
+    const parrafo = document.createElement("p");
+    parrafo.textContent = `${titulo} - ${descripcion} `;
+    const hr = document.createElement("hr");
+    divPublicaciones.appendChild(parrafo);
+    divPublicaciones.appendChild(hr);
+    console.log();
+  });
+}
+
+function limpiarObjeto() {
+  objPublicacion.titulo = "";
+  objPublicacion.descripcion = "";
+}
+function refrescarHTML() {
+  const divPublicaciones= document.querySelector("#publicacion-div");
+  while (divPublicaciones.firstChild) 
+  {
+    divPublicaciones.removeChild(divPublicaciones.firstChild);
+  }
+}
